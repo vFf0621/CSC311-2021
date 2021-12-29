fd5804e9e3fc92c3405d4cd12411dfa5fd6fdf13
vFf0621 <victorkwan1206@yahoo.com>
2021-12-28 20:26:28 -0500

    First Commit

---
+++ hw2_code/q3/run_logistic_regression.py
@@ -0,0 +1,80 @@
+from check_grad import check_grad
+from utils import *
+from logistic import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def run_logistic_regression():
+    train_inputs, train_targets = load_train()
+    # train_inputs, train_targets = load_train_small()
+    valid_inputs, valid_targets = load_valid()
+
+    N, M = train_inputs.shape
+
+    #####################################################################
+    # TODO:                                                             #
+    # Set the hyperparameters for the learning rate, the number         #
+    # of iterations, and the way in which you initialize the weights.   #
+    #####################################################################
+    hyperparameters = {
+        "learning_rate": 0.001,
+        "weight_regularization": 0.,
+        "num_iterations": 10000
+    }
+    weights = np.random.uniform(size=(M + 1, 1))
+    #####################################################################
+    #                       END OF YOUR CODE                            #
+    #####################################################################
+
+    # Verify that your logistic function produces the right gradient.
+    # diff should be very close to 0.
+    run_check_grad(hyperparameters)
+
+    # Begin learning with gradient descent
+    #####################################################################
+    # TODO:                                                             #
+    # Modify this section to perform gradient descent, create plots,    #
+    # and compute test error.                                           #
+    #####################################################################
+    for t in range(hyperparameters["num_iterations"]):
+        f, df, y = logistic(weights, train_inputs, train_targets, 
+                            hyperparameters)    
+        # w -= a * dw
+        weights -= df.T * hyperparameters["learning_rate"]
+        
+        plt.plot(t, f[0], "ro")
+        plt.plot(t, f[1], "go")
+            
+    #####################################################################
+    #                       END OF YOUR CODE                            #
+    #####################################################################
+
+
+def run_check_grad(hyperparameters):
+    """ Performs gradient check on logistic function.
+    :return: None
+    """
+    # This creates small random data with 20 examples and
+    # 10 dimensions and checks the gradient on that data.
+    num_examples = 20
+    num_dimensions = 10
+
+    weights = np.random.randn(num_dimensions + 1, 1)
+    data = np.random.randn(num_examples, num_dimensions)
+    targets = np.random.rand(num_examples, 1)
+
+    diff = check_grad(logistic,
+                      weights,
+                      0.001,
+                      data,
+                      targets,
+                      hyperparameters)
+
+    print("diff =", diff)
+
+
+if __name__ == "__main__":
+    run_logistic_regression()
+  #  run_pen_logistic_regression()
